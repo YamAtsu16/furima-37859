eafd3dfe2d3e281fffaaaa8e18b8f38ecc6442c6
YamAtsu16 <00kisaragi16@gmail.com>
2022-05-11 20:57:30 +0900

    エラー文及びバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :name, :text, :price, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_date_id, presence: true
-  validates :price, format: { with: /\A[0-9]+\z/ }, numericality: {in: 300..9999999}
-  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 1 }
+  validates :name, :text, :image, :price, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_date_id, presence: true
+  validates :price, format: { with: /\A[0-9]+\z/ }, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 1, message: "can't be blank" }
 end
