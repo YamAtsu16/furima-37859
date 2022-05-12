bb50038ee65d5a31367b48071311b66d82e6bb70
YamAtsu16 <00kisaragi16@gmail.com>
2022-05-12 12:09:48 +0900

    モデル単体テストコード（item）の実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,75 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  context '商品出品ができるとき' do
+    it '入力項目が全て入力されていると登録できる' do
+      expect(@item).to be_valid
+    end
+  end
+  context "商品出品ができないとき" do
+    it 'nameが空だと出品できない' do
+      @item.name = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+    it 'textが空だと出品できない' do
+      @item.text = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Text can't be blank")
+    end
+    it 'imageが空だと出品できない' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+    it 'priceが空だと出品できない' do
+      @item.price = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+    it 'category_idが空だと出品できない' do
+      @item.category_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank")
+    end
+    it 'condition_idが空だと出品できない' do
+      @item.condition_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition can't be blank")
+    end
+    it 'delivery_charge_idが空だと出品できない' do
+      @item.delivery_charge_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
+    end
+    it 'prefecture_idが空だと出品できない' do
+      @item.prefecture_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+    end
+    it 'shipping_date_idが空だと出品できない' do
+      @item.shipping_date_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping date can't be blank")
+    end
+    it 'priceが半角数字でないと出品できない' do
+      @item.price = Faker::Lorem.sentence
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
+    it 'priceが300以上でないと出品できない' do
+      @item.price = Faker::Number.between(to: 299) 
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+    end
+    it 'priceが9999999以下でないと出品できない' do
+      @item.price = Faker::Number.between(from: 100000000) 
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+    end
+  end
 end
