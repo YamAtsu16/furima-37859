3651fbd4971504bb924a64b176204246cc2769a5
YamAtsu16 <00kisaragi16@gmail.com>
2022-05-13 16:56:15 +0900

    items_controller内のコード整理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: :edit
+  before_action :find_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(params[:id])
     else
@@ -47,5 +45,9 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
 
     redirect_to root_path if current_user.id != @item.user_id
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 end
