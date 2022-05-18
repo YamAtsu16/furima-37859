f1104225794cc20c831046ee5069a96c89b949a7
YamAtsu16 <00kisaragi16@gmail.com>
2022-05-18 16:29:51 +0900

    S3の導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_12_151247) do
+ActiveRecord::Schema.define(version: 2022_05_12_150332) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
